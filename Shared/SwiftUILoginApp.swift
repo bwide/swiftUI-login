5c4766b8c7b765d25fd75c7d2b8fe690b4d94411
bwide <bfwide07@gmail.com>
2021-12-25 21:50:36 -0300

    Initial Commit

---
+++ Shared/SwiftUILoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILoginApp.swift
+//  Shared
+//
+//  Created by Bruno Wide on 25/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
